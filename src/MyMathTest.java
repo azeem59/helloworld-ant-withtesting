e23fdd671ac90061d6b20c548fdb21f20e53208d
azeem59 <azeem59@gmail.com>
2017-09-21 13:44:27 +0200

    b

--- src/MyMathTest.java
+++ src/MyMathTest.java
@@ -1,6 +1,6 @@
+package test;
 
-
-
+import math.MyMath;
 
 import org.junit.Test;
 
@@ -9,7 +9,6 @@ import static org.junit.Assert.assertEquals;
 public class MyMathTest {
     @Test
     public void testMulti() {
-        MyMath math = new MyMath();
-        assertEquals(50, math.multi(5, 10));
+       
     }
 }
